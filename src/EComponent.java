4ac9cdb36eae0e5113a03ce7707864f2554ffb73
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-05-22 16:21:40 +0200

    Created Application with main method. Created enum EComponent. Created GUI class without implementation.

---
+++ src/EComponent.java
@@ -0,0 +1,3 @@
+public enum EComponent {
+    fuel, oilTemperature, oilPressure, wheelPressure, gear
+}
---
+++ src/GUI.java
@@ -0,0 +1,7 @@
+public class GUI {
+    private EComponent currDisplayedData;
+
+    public void display(EComponent componentToDisplay) {
+
+    }
+}
