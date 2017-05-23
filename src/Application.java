91da9d93f9c552dae6d85725b820fc09a10f9d96
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-05-23 19:23:30 +0200

    Application randomizes data to display 10 times.

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,20 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        GUI gui = new GUI();
+        Random random = new Random();
+        for(int i = 0;i<10;++i){
+            EComponent desiredComponent = EComponent.values()[random.nextInt(EComponent.values().length)];
+            gui.display(desiredComponent);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                System.out.println("Sleep interrupted");
+            }
+        }
     }
 
 }
