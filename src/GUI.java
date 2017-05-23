884966533133b90761cfe9e5d09dfd76fea4dc25
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-05-23 19:08:42 +0200

    GUI now has CarData variable, as well as a display method.

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,11 @@
 public class GUI {
     private EComponent currDisplayedData;
-
+    private CarData carData = new CarData();
     public void display(EComponent componentToDisplay) {
-
+        if(componentToDisplay != currDisplayedData) {
+            currDisplayedData = componentToDisplay;
+            System.out.println("Now displaying: " + currDisplayedData);
+            System.out.println(carData.get(componentToDisplay).getData());
+        }
     }
 }
